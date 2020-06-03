c3c98a0d84c45c93c7e7d72efa63c2cd308ac6f9
tomyre <tom.larnicol@edu.ece.fr>
2020-06-03 17:05:01 +0200

    update operation bdd

--- Emploi du tempsV2/src/controller/Connexion.java
+++ Emploi du tempsV2/src/controller/Connexion.java
@@ -18,17 +18,12 @@ import javax.swing.JOptionPane;
  */
 public class Connexion {
 
-    /**
-     * Attributs prives : connexion JDBC, statement, ordre requete et resultat
-     * requete
-     */
     private final Connection conn;
-    private Statement stmt;
-    private ResultSet rset = null, res;
+    private final Statement stmt;
+    private ResultSet rset = null;
     private ResultSetMetaData rsetMeta;
-    private String requete,query;
-
-
+    private String requete;
+    private int ID,res;
 
     /**
      * Constructeur avec 3 paramètres : nom, login et password de la BDD locale
@@ -66,14 +61,11 @@ public class Connexion {
                 boolean motDepasseValide=(mdpBD.equals(mdp));
                 if(motDepasseValide)
                 {
-                    return rset.getInt("DROIT");
-                   
+                    return rset.getInt("DROIT");                   
                 } 
                 else
                 {
-                    // recommncer loperation de connexion
                     JOptionPane.showMessageDialog(null, "Identifiants ou password incorrects try again", "Erreur", JOptionPane.ERROR_MESSAGE);
-                    //System.out.println("Identifiants ou password incorrects try again");
                 }
             }
         } catch (SQLException e){
@@ -81,8 +73,102 @@ public class Connexion {
 
         }
         return -1;
+   }  
+
+    public int RecupererID(String email, String mdp) throws SQLException{
+  
+      requete = "SELECT * FROM utilisateur WHERE EMAIL='"+email+"'";
+    
+        try{
+            rset =stmt.executeQuery(requete);
+            rset.next();
+            if(rset!=null)
+            {
+                String mdpBD=rset.getString("PASSWD");
+                boolean motDepasseValide=(mdpBD.equals(mdp));
+                if(motDepasseValide)
+                {
+                    return rset.getInt("ID");
+                     
+                } 
+                else
+                {
+                    
+                    System.out.println("erreur"); //JOptionPane.showMessageDialog(null, "Identifiants ou password incorrects try again", "Erreur", JOptionPane.ERROR_MESSAGE);
+                    //System.out.println("Identifiants ou password incorrects try again");
+                }
+            }
+        } catch (SQLException e){
+            System.out.println("erreur");//JOptionPane.showMessageDialog(null, "Identifiants ou password incorrects try again", "Erreur", JOptionPane.ERROR_MESSAGE);
+
+        }
+        return -1;
         
-        /*try{
+        
+    }
+    
+    public void OperationMofidication(String table,String colonne,String ID, String modif){
+              
+        requete = "UPDATE "+table+" SET "+colonne+" = '"+modif+"' WHERE ID = '"+ID+"'";
+               
+        try{
+            res =stmt.executeUpdate(requete);
+            if(res==1)
+            {
+                JOptionPane.showMessageDialog(null, "Informations modifié", "Info", JOptionPane.INFORMATION_MESSAGE);
+            } 
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Erreur  dans les saisies veuillez vous assurer que vous ne vous etes pas trompé", "ERREUR", JOptionPane.ERROR_MESSAGE);
+            }
+        }catch (SQLException e){
+        JOptionPane.showMessageDialog(null, "Erreur  dans les saisies veuillez vous assurer que vous ne vous etes pas trompé", "ERREUR", JOptionPane.ERROR_MESSAGE);
+        }          
+   }
+    
+    
+   public void OperationSuppression(String table,String ID){
+       
+       requete = "DELETE FROM "+table+" WHERE 'ID' = '"+ID+"'"; 
+       System.out.println(requete);
+        
+        try{
+            PreparedStatement pst = conn.prepareStatement(requete); 
+            int res = pst.executeUpdate();
+            //res = stmt.executeUpdate(requete);
+            if(res!=1)
+            {
+                JOptionPane.showMessageDialog(null, "Erreur 2: valeur retour: "+res, "ERREUR", JOptionPane.ERROR_MESSAGE);
+            } 
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Site supprimé", "Info", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }catch (SQLException e){
+        JOptionPane.showMessageDialog(null, "Erreur 1 probleme de syntaxe dans la requete SQL", "ERREUR", JOptionPane.ERROR_MESSAGE);
+        }          
+   }       
+
+    public void OperationInsertionCours(String table,String NomCours){
+        requete = "INSERT INTO "+table+"(NOM) VALUES ('"+ID+"')";
+        
+        try{
+            res=stmt.executeUpdate(requete);
+            if(res==1)
+            {
+                JOptionPane.showMessageDialog(null, "Le cours à éte ajouté", "Info", JOptionPane.INFORMATION_MESSAGE);
+            } 
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Erreur 2 dans les saisies veuillez vous assurer que vous ne vous etes pas trompé", "ERREUR", JOptionPane.ERROR_MESSAGE);
+            }
+        }catch (SQLException e){
+        JOptionPane.showMessageDialog(null, "Erreur 1 dans les saisies veuillez vous assurer que vous ne vous etes pas trompé", "ERREUR", JOptionPane.ERROR_MESSAGE);
+        }          
+   }  
+}
+   
+/*try{
             rsetMeta=rset.getMetaData();
          
             //int nbCols = rsetMeta.getColumnCount();
@@ -98,7 +184,3 @@ public class Connexion {
         }catch(SQLException e){
           System.out.println(e.getMessage()); 
         }*/
-    }
-}
-        
-      
---
+++ Emploi du tempsV2/src/controller/Operation.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tom
+ */
+public class Operation {
+    
+    private final Connection conn;
+    private final Statement stmt;
+    private ResultSet rset = null;
+    private ResultSetMetaData rsetMeta;
+    private String requete;
+    private int res;
+    
+    
+    public void OperationMofidication(String table,String colonne,String ID, String modif){
+              
+        requete = "UPDATE "+table+" SET "+colonne+" = '"+modif+"' WHERE ID = '"+ID+"'";
+               
+        try{
+            res =stmt.executeUpdate(requete);
+            if(res==1)
+            {
+                JOptionPane.showMessageDialog(null, "Informations modifié", "Info", JOptionPane.INFORMATION_MESSAGE);
+            } 
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Erreur  dans les saisies veuillez vous assurer que vous ne vous etes pas trompé", "ERREUR", JOptionPane.ERROR_MESSAGE);
+            }
+        }catch (SQLException e){
+        JOptionPane.showMessageDialog(null, "Erreur  dans les saisies veuillez vous assurer que vous ne vous etes pas trompé", "ERREUR", JOptionPane.ERROR_MESSAGE);
+        }          
+   }
+    
+ public void OperationInsertionCours(String table,String NomCours){
+        requete = "INSERT INTO "+table+"(NOM) VALUES ('"+NomCours+"')";
+        
+        try{
+            res=stmt.executeUpdate(requete);
+            if(res==1)
+            {
+                JOptionPane.showMessageDialog(null, "Le cours à éte ajouté", "Info", JOptionPane.INFORMATION_MESSAGE);
+            } 
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Erreur 2 dans les saisies veuillez vous assurer que vous ne vous etes pas trompé", "ERREUR", JOptionPane.ERROR_MESSAGE);
+            }
+        }catch (SQLException e){
+        JOptionPane.showMessageDialog(null, "Erreur 1 dans les saisies veuillez vous assurer que vous ne vous etes pas trompé", "ERREUR", JOptionPane.ERROR_MESSAGE);
+        }          
+   }  
+}
